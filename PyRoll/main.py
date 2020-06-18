67d7ef2ea8b9713bd22ee14a153a49cc64f4e939
Bobby Huffstetler <63428077+bhuffstetler@users.noreply.github.com>
2020-06-18 08:09:20 -0400

    Update main.py

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -1,69 +1,50 @@
-
-#import file
-
 import os
 import csv
 
-#path to the csvfile
+cvspath = os.path.join("election_data.csv)
 
-csvpath = os.path.join('budget_data.csv')
+# variables
+poll_data={}
+total_votes = 0
+with open(csvpath, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+    next(csvread, None)
 
-#initializing the variables 
-total_months = 0
-total_revenue =0
-changes =[]
-date_count = []
-greatest_inc = 0
-greatest_inc_month = 0
-greatest_dec = 0
-greatest_dec_month = 0
+    for row in cvsread:
+        total_votes += 1
+        if row[2] in poll_data.keys():
+            pool_data[row{2]} = poll_data[row[2]] + 1
+            else:
+                poll_data[row[2]] = 1
 
-# Open the CSV
-with open(csvpath, newline = '') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    next(csvreader, None)
-    row = next(csvreader)
-# calculating the total number of months and total revenue
-    previous_profit = int(row[1])
-    total_months = total_months + 1
-    total_revenue = total_revenue + int(row[1])
-    greatest_inc = int(row[1])
-    greatest_inc_month = row[0]
+candidates = []
+total_number_votes = []
 
-    for row in csvreader:
- 
-        total_months = total_months + 1
-        total_revenue = total_revenue + int(row[1])
+for key, value in poll_data.items():
+candidates.append(key)
+total_number_votes.append(value)
 
-        # Calculate change from this month to previous months
-        change = int(row[1]) - previous_profit
-        changes.append(change)
-        previous_profit = int(row[1])
-        date_count.append(row[0])
-        
-        #calculating the greatest increase
-        if int(row[1]) > greatest_inc:
-            greatest_inc = int(row[1])
-            greatest_inc_month = row[0]
-            
-        #calculating the greatest decrease
-        if int(row[1]) < greatest_dec:
-            greatest_dec = int(row[1])
-            greatest_dec_month = row[0]  
-      
-    # calculating the average and date
-    average_change = sum(changes)/len(changes)
+# Percent of votes 
+percantage_votes =[]
+for n in total_number_votes:
+    percentage_votes.append(round(n/total_votes * 100, 1))
 
-    high = max(changes)
-    low = min(changes)
+# find the winner
+clean_data = list(zip(candidates, total_number_votes, percentage_votes))
 
-    # printing all values
-    print("Financial Analysis")
-    print("Total Months:" + str(total_months))
-    print("Total Amount:" + str(total_revenue))
-    print(average_change)
-    print(greatest_inc_month, max(changes))
-    print(greatest_dec_month, min(changes))
+winner_list = []
+for name in clean_data:
+    if max(total_number_votes) == name[1]:
+        winner_list.append(name[0])
+winner = winner_list[0]
+
+# Print data 
+print("Election results :")
+print(total_votes)
+prunt(candidates)
+print(percentage_votes)
+print(total_number_votes)
+print(winner)
 
 # output files
 PyRoll = open("output.txt","w+")
